a7cfaeede2e1546a53c709df34f0c23e6c637e1d
daltondiaz <d_tecnologia@yahoo.com.br>
2022-04-16 23:41:47 -0300

    refactor: move routes to a file

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"github.com/daltondiaz/automatic-train/bitbucket"
-	"github.com/gin-gonic/gin"
+	"github.com/daltondiaz/automatic-train/router"
 )
 
 func main() {
-	router := gin.Default()
-	router.POST("/v1/bitbucket", getBitbucketPush)
-	router.Run("localhost:9191")
-}
-
-func getBitbucketPush(ctx *gin.Context) {
-	var webhook bitbucket.Webhook
-
-	if err := ctx.BindJSON(&webhook); err != nil {
-		return
-	}
-	log.Printf("%v", webhook)
-
+	router.Router()
 }
